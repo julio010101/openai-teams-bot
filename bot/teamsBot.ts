587d2cb5ae9cf706ba3437ae47fe914e7ddc158d
JULIO CESAR CHAVES <julio.010101@gmail.com>
2023-03-06 17:27:28 -0300

    adaptações ic

--- bot/teamsBot.ts
+++ bot/teamsBot.ts
@@ -9,15 +9,16 @@ import rawWelcomeCard from "./adaptiveCards/welcome.json";
 import { AdaptiveCards } from "@microsoft/adaptivecards-tools";
 import { Configuration, OpenAIApi } from "openai";
 import config from "./config";
+import axios from "axios";
 
 export class TeamsBot extends TeamsActivityHandler {
   constructor() {
     super();
 
-    const configuration = new Configuration({
-      apiKey: config.openaiApiKey,
-    });
-    const openai = new OpenAIApi(configuration);
+    //const configuration = new Configuration({
+    //  apiKey: config.openaiApiKey,
+    //});
+    //const openai = new OpenAIApi(configuration);
 
     this.onMessage(async (context, next) => {
       console.log("Running with Message Activity.");
@@ -29,12 +30,23 @@ export class TeamsBot extends TeamsActivityHandler {
         txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
       }
 
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: txt,
-        temperature: 0,
-        max_tokens: 2048,
-      });
+      //const response = await openai.createCompletion({
+      //  model: "text-davinci-003",
+      //  prompt: txt,
+      //  temperature: 0,
+      //  max_tokens: 2048,
+      //});
+
+   const response = await axios.post("https://alexacpdoc.openai.azure.com/openai/deployments/dicionariohistorico/completions?api-version=2022-12-01",{
+     prompt: txt, 
+		temperature: 0.7, 
+		max_tokens: 2048
+		}, {
+		 headers: { 
+			"Content-Type": "application/json", 
+			"api-key": "216dec44a24a4b8c8984f1488cb48915" 
+			} 
+		})
 
       await context.sendActivity(response.data.choices[0].text);
 
